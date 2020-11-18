acda11667bf384983e4370a0cd0539a8c21ae5d3
Mellissa Basham <mellbasham@gmail.com>
2020-11-18 03:40:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,15 +6,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  
+  puts "subtraction"
+  (num1 - num2)
 end
 
 def division(num1, num2)
-  
+  puts "division"
+  (num1 \ num2)
 end
 
 def multiplication(num1, num2)
-  
+  puts "multiplication"
+  (num1 * num2)
 end
 
 def modulo(num1, num2)
